66f825ae4af2bc48d30264e5d5d84b01fb8d4514
Wajud <wajudkareem757@gmail.com>
2024-09-12 14:49:03 +0100

    implemented sort and pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,39 @@
+const Product = require("../models/product");
+const { search } = require("../routes/products");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("Testing async errors");
-  res.status(200).json({ message: "Product Testing Route" });
+  const products = await Product.find({}).select("name price");
+  res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ message: "Product Route" });
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join("");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  if (fields) {
+    const fieldsList = fields.split(",").join("");
+    result = result.select(fieldsList);
+  }
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
